0182bccf8477fa68df76b01d9741f02a1d51a5ee
Bob Bobov <bobyy@yandex.ru>
2018-12-26 12:47:11 +0200

    erer

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Movie } from '../shared/models/movie';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  private baseUrl = 'http://www.omdbapi.com?plot=full';
+  private apiKey ="2e9cd746";
+  private movieId='tt';
+
+  constructor(private http: HttpClient) {
+
+   }
+
+  getMovieById(id:string):Observable<Movie>{
+
+    const url = this.baseUrl + '&' + 'tt1234567' + this.apiKey;
+   let test=this.http.get<Movie>(url);
+   test.subscribe(res=>
+    {
+      debugger;
+    });
+    return  test;
+  }
+
 }
---
+++ src/app/shared/models/movie.ts
@@ -0,0 +1,29 @@
+import { Rating } from "./rating";
+
+export interface Movie{
+
+    title: string;
+    year: number;
+    rated: string;
+    released: Date;
+    season: number;
+    episode: number;
+    runtime: string;
+    genre: string
+    director: string;
+    writer: string
+    actors: string;
+    plot: string;
+    language: string;
+    country: string;
+    awards: string;
+    poster: string;
+    ratings:Array<Rating>;
+    metascore: string;
+    imdbRating: number
+    imdbVotes: number
+    imdbID: string;
+    seriesID: string;
+    type: string;
+    response: boolean
+}
