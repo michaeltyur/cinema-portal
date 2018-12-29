62ee09a2459d5750f4b17f1f87b366205c639d6e
Bob Bobov <bobyy@yandex.ru>
2018-12-29 02:32:57 +0200

    trtrtr

--- src/app/shared/models/consts.ts
+++ src/app/shared/models/consts.ts
@@ -2,17 +2,17 @@ export const moviesName: string[]=['The Wizard of Oz',
                          'Citizen Kane',
                          'Get Out',
                           'The Green Mile',
-                        //  'The Mist',
-                        //  'Thinner',
-                        //  'The Shawshank Redemption',
-                        //  'Stand by Me',
-                        //  'The Shining',
-                        //  'Pet Sematary',
-                        //  'It',
-                        //  'The Dead Zone',
-                        //  'Secret Window',
-                        //  'The Running Man',
-                        //  "Salem's Lot"
+                         'The Mist',
+                         'Thinner',
+                         'The Shawshank Redemption',
+                         'Stand by Me',
+                         'The Shining',
+                         'Pet Sematary',
+                         'It',
+                         'The Dead Zone',
+                         'Secret Window',
+                         'The Running Man',
+                         "Salem's Lot"
                     ];
 export const yearMinValue:number=1900;
 export const yearMaxValue:number=2019;
